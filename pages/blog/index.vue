189487599d3270c7e1ea33eb9e786e0452985f3c
David Chen <dchen07@protonmail.com>
2022-08-23 22:00:55 -0400

    Add preliminary post link
    This gives a route for the crawler to detect the page

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -5,8 +5,17 @@
 	so we can't use `index.md` reliably.
 -->
 <template>
+  <nav>
+    <ContentNavigation v-slot="{ navigation }">
+      <div v-for="blog of navigation" :key="blog._path">
+        <div v-for="post of blog.children" :key="post._path">
+          <NuxtLink :to="post._path">{{ post.title }}</NuxtLink>
+      	</div>
+      </div>
+    </ContentNavigation>
+  </nav>
   <main>
     Welcome to the blog!
-    A catalog page will appear here eventually...
+    A properly designed catalog page will appear here eventually...
   </main>
 </template>
