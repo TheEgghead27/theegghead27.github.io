1a8003ec1719ca943520018b7646497198c956e7
David Chen <dchen07@protonmail.com>
2022-08-23 21:31:27 -0400

    Add Nuxt3 content-based blog config
    Runs `npx nuxi init theegghead27.github.io -t content` to create a
    content-based `/blog/` section of the site, which reads Markdown entries
    from `content/blog/`.
    
    Adds some placeholder index content, along with an initial blog entry.

---
+++ pages/blog/index.vue
@@ -0,0 +1,12 @@
+<!-- 
+	Blog index gets its own page design
+	
+	<ContentDoc> uses the first content it finds,
+	so we can't use `index.md` reliably.
+-->
+<template>
+  <main>
+    Welcome to the blog!
+    A catalog page will appear here eventually...
+  </main>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,4 @@
+<template>
+  <h1>Hello site index!</h1>
+  <p>Welcome to my little corner of the internet!</p>
+</template>
