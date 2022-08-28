c199b9aa2ca0674945a35650afc18a3e182cbc61
David Chen <dchen07@protonmail.com>
2022-08-27 21:52:41 -0400

    A little styling

--- pages/blog/[...slug].vue
+++ pages/blog/[...slug].vue
@@ -3,3 +3,20 @@
     <ContentDoc />
   </main>
 </template>
+
+<style>
+main {
+	padding: 0 20vw;
+}
+main img {
+	width: 100%;
+}
+main pre {
+	color: white;
+	background-color: rgba(0,0,0,0.7);
+	padding: 8px;
+	width: 90%;
+	overflow: scroll;
+	border-radius: 8px;
+}
+</style>
